d135e2f9420fe2cf2908d1c30d83d3603196be45
Евгений Евгений <ypssyp1@outlook.com>
2020-01-16 20:59:22 +0300

    Добавлено модульное тестирование для контроллера пользователя

--- CodeBlogFitness1.BL/Controller/UserController.cs
+++ CodeBlogFitness1.BL/Controller/UserController.cs
@@ -42,7 +42,7 @@ namespace CodeBlogFitness1.BL.Controller
                 Users.Add(CurrentUser);
                 IsNewUser = true;
                 Save();
-            }
+            }                                                   
             
         }
                  /// <summary>
@@ -56,7 +56,7 @@ namespace CodeBlogFitness1.BL.Controller
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
 
-                if (formatter.Deserialize(fs) is List<User> users)
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> users) 
                 {
                     return users;
                 }
